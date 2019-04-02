1a575e659b566c860784137e801544cce3c897a7
Christian Ransom <christian.ransom123@gmail.com>
2019-04-02 08:51:37 -0700

    Committing code directly to the Master branch

---
+++ master_branch_feature.py
@@ -0,0 +1,15 @@
+
+
+
+ 
+# define a main function
+def main():
+    
+    print("I put this right into the master branch")
+    
+    
+
+if __name__=="__main__":
+    # call the main function
+    main()
+ 
